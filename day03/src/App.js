b02419b53966ce76be59eb4fcbd5536a456f7150
moumith-bingo <moumith.uki.stu@dreamspace.academy>
2025-10-15 10:09:32 +0530

    New inputs

---
+++ day03/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 style={{color:"pink",padding:30,textAlign:"center",fontFamily:"monospace"}}>New Data</h1>
+    <p style={{textAlign:"center",color:"green",fontFamily:"sans-serif",fontSize:20}}>You're a genius the morning light is turning blue the feeling is bizzare,the night is almost over still don't know where you are.The shadows,yeah they make me pretty like movie star,the daylight make me feel like dracula In the end I hope It's you and me</p>
+    </>
+  );
+}
+
+export default App;
---
+++ day03/src/components/Day2.jsx
@@ -0,0 +1,13 @@
+import Day1 from "../../../day01/src/components/Day1"
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <h1>you're a genius morning light is turning blue feeling is bizzare</h1>
+</body>
+</html>
